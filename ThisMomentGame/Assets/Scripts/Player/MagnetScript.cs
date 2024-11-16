9a828e7ee25b1d495fb8e69d8bb00bb4c2ceb07c
Mason Crochetiere <mason.crochetiere@mymail.champlain.edu>
2024-11-16 02:48:05 -0500

    Added magnet pull mechanism

---
+++ ThisMomentGame/Assets/Scripts/Player/MagnetScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetScript : MonoBehaviour
+{
+    PlayerMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponentInParent<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ADDING MAG TARGET");
+        movement.GetMagnetTarget(other.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("BYE MAG TARGET");
+        movement.RemoveMagnetTarget(other.gameObject);
+    }
+}
--- ThisMomentGame/Assets/Scripts/Player/PlayerMovement.cs
+++ ThisMomentGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.DefaultInputActions;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 7f;
-    public float maxSpeed = 2f;
+    [Header("Move Variables")]
+    [SerializeField] float moveSpeed = 7f;
+    [SerializeField] float maxSpeed = 2f;
+
+    float baseDrag;
+
+    [Header("Magnet Variables")]
+    [SerializeField] float magnetRadius = 5f;
+    [SerializeField] float magnetAngle = 0.3f;
+
+    [SerializeField] float maxMagnetPull = 2f;
+    [SerializeField] float currentMagnetPull = 0f;
+    [SerializeField] float magnetDrag = 0.3f;
+
+    GameObject magnetTarget = null;
 
     PlayerActions actions;
     PlayerActions.PlayerMovementActions playerMovement;
@@ -27,10 +41,20 @@ public class PlayerMovement : MonoBehaviour
         playerMovement.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>(); // moveInput = 
 
         rb = GetComponent<Rigidbody2D>();
+        baseDrag = rb.drag;
     }
 
     private void FixedUpdate()
     {
+        currentMagnetPull = 0;
+        if (magnetTarget != null)
+        {
+            // so basically the magnet pull is an extra maximum force addition 
+            // we're calculating it here based on how close the player is to the target
+            currentMagnetPull = Mathf.Lerp(0, maxMagnetPull, 
+                (1 - Vector3.Distance(transform.position, magnetTarget.transform.position) / magnetRadius));
+        }
+
         Move();
     }
 
@@ -38,7 +62,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (moveInput != Vector2.zero)
         {
-            Debug.Log("MOVE IS " + moveInput);
+            //Debug.Log("MOVE IS " + moveInput);
         }
 
         // These if statements are making redirecting movement go faster
@@ -57,11 +81,23 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(moveInput * moveSpeed);
 
         // clamp to a max speed to keep the acceleration on the move without a big mess
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);     
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, (maxSpeed + currentMagnetPull));     
 
         // our deceleration is handled with the Rigidbody's Linear Drag value right now. 
-
     }
 
+    public void GetMagnetTarget(GameObject target)
+    {
+        magnetTarget = target;
+        rb.drag = magnetDrag;
+    }
 
+    public void RemoveMagnetTarget(GameObject target)
+    {
+        if (magnetTarget == target)
+        {
+            magnetTarget = null;
+            rb.drag = baseDrag;
+        }
+    }
 }
