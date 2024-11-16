dde625ffede34cd8eba0352a583d3499667ccd74
adam.gyenes <adam.gyenes03@gmail.com>
2024-11-16 14:21:03 -0500

    Color transition

--- ThisMomentGame/Assets/Scripts/Player/ColorChanger.cs
+++ ThisMomentGame/Assets/Scripts/Player/ColorChanger.cs
@@ -32,26 +32,32 @@ public class ColorChanger : MonoBehaviour
     {
         AssignNewColor(Color.red);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         AssignNewColor(Color.blue);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         AssignNewColor(new Color(0.5f, 0.5f, 0f));
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         AssignNewColor(Color.green);
     }
 
-    private void AssignNewColor(Color color, float alpha = 1f)
+    private void AssignNewColor(Color color, float transitionDuration = 0.5f, float alpha = 1f)
     {
+        Material oldMaterial = targetSprtie.material;
+        Color oldColor = oldMaterial.GetColor("_NextColor");
+
         Material newColorMat = new(multicolorShader);
 
         newColorMat.name = "TransientColorMaterial";
-        newColorMat.SetColor("_Color", color);
+        newColorMat.SetColor("_PreviousColor", oldColor);
+        newColorMat.SetColor("_NextColor", color);
         newColorMat.SetFloat("_Alpha", alpha);
+        newColorMat.SetFloat("_TransitionDuration", transitionDuration);
+        newColorMat.SetFloat("_TransitionStart", Time.time);
 
         targetSprtie.material = newColorMat;
     }
