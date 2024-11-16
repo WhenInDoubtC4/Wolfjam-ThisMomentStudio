3e87a58658e129cebb6ab331e8669cec699d1fd6
James Johnson <james.johnson01@mymail.champlain.edu>
2024-11-16 15:21:47 -0500

    Added UI to tutorial

---
+++ ThisMomentGame/Assets/Scripts/Tweening/FadeUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeUI : MonoBehaviour
+{
+    [SerializeField] List<Image>
+
+    public void FadeIn()
+    {
+
+    }
+
+    public void FadeOut()
+    {
+
+    }
+
+
+}
